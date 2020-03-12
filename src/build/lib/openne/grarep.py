d4b9dd42fc008ac55d917f4e98e24d5bf0a64591
Bznkxs <gogodu@gmail.com>
2020-03-12 22:43:23 +0800

    chkoutall&reinstall (local)

--- src/build/lib/openne/grarep.py
+++ src/build/lib/openne/grarep.py
@@ -1,6 +1,9 @@
 import math
-import numpy as np
-from numpy import linalg as la
+# import numpy as np
+# from numpy import linalg as la
+
+import torch
+
 from sklearn.preprocessing import normalize
 
 
@@ -14,29 +17,35 @@ class GraRep(object):
         self.train()
 
     def getAdjMat(self):
-        graph = self.g.G
+        # graph = self.g.G
         node_size = self.g.node_size
         look_up = self.g.look_up_dict
-        adj = np.zeros((node_size, node_size))
+        # adj = np.zeros((node_size, node_size))
+        adj = torch.zeros((node_size, node_size)) # a symmetric matrix
         for edge in self.g.G.edges():
             adj[look_up[edge[0]]][look_up[edge[1]]] = 1.0
             adj[look_up[edge[1]]][look_up[edge[0]]] = 1.0
         # ScaleSimMat
-        return np.matrix(adj/np.sum(adj, axis=1))
+        return adj/torch.sum(adj, 1)
+        # return np.matrix(adj/np.sum(adj, axis=1))
 
     def GetProbTranMat(self, Ak):
-        probTranMat = np.log(Ak/np.tile(
-            np.sum(Ak, axis=0), (self.node_size, 1))) \
-            - np.log(1.0/self.node_size)
+        probTranMat = torch.log(Ak/torch.Tensor.repeat(
+            Ak.sum(0),(self.node_size,1))) \
+            - torch.log(1.0/self.node_size)
+       # probTranMat = np.log(Ak/np.tile(
+       #     np.sum(Ak, axis=0), (self.node_size, 1))) \
+       #     - np.log(1.0/self.node_size)
         probTranMat[probTranMat < 0] = 0
-        probTranMat[probTranMat == np.nan] = 0
+        probTranMat[torch.isnan(probTranMat)] = 0
         return probTranMat
 
     def GetRepUseSVD(self, probTranMat, alpha):
-        U, S, VT = la.svd(probTranMat)
+        U, S, VT = torch.svd(probTranMat)  # la.svd(probTranMat)
         Ud = U[:, 0:self.dim]
         Sd = S[0:self.dim]
-        return np.array(Ud)*np.power(Sd, alpha).reshape((self.dim))
+        return torch.as_tensor(Ud)*torch.pow(Sd, alpha).reshape(self.dim)
+        # return np.array(Ud)*np.power(Sd, alpha).reshape((self.dim))
 
     def save_embeddings(self, filename):
         fout = open(filename, 'w')
@@ -49,11 +58,12 @@ class GraRep(object):
     def train(self):
         self.adj = self.getAdjMat()
         self.node_size = self.adj.shape[0]
-        self.Ak = np.matrix(np.identity(self.node_size))
-        self.RepMat = np.zeros((self.node_size, int(self.dim*self.Kstep)))
+        self.Ak = torch.eye(self.node_size)  # np.matrix(np.identity(self.node_size))
+        self.RepMat = torch.zeros((self.node_size, int(self.dim * self.Kstep)))
+        #                np.zeros((self.node_size, int(self.dim*self.Kstep)))
         for i in range(self.Kstep):
             print('Kstep =', i)
-            self.Ak = np.dot(self.Ak, self.adj)
+            self.Ak = torch.mm(self.Ak, self.adj) #  np.dot(self.Ak, self.adj)
             probTranMat = self.GetProbTranMat(self.Ak)
             Rk = self.GetRepUseSVD(probTranMat, 0.5)
             Rk = normalize(Rk, axis=1, norm='l2')
