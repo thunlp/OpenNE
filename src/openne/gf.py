d4249ef9bcb304527313d925a5fd23689d98ad9c
Bznkxs <gogodu@gmail.com>
2020-03-14 12:54:52 +0800

    gf bug fix

--- src/openne/gf.py
+++ src/openne/gf.py
@@ -40,16 +40,17 @@ class GraphFactorization(object):
 
     def get_train(self):
 
+
         adj_mat = self.adj_mat
+        mat_mask = torch.as_tensor(adj_mat > 0, dtype=torch.float32)
 
-        mat_mask = 1.*(adj_mat > 0)
-
-        _embeddings = Variable(torch.nn.init.xavier_normal_(torch.FloatTensor(self.node_size, self.rep_size)), requires_grad=True)
+        _embeddings = Variable(torch.nn.init.xavier_uniform_(torch.FloatTensor(self.node_size, self.rep_size)), requires_grad=True)
         # _embeddings = tf.Variable(tf.contrib.layers.xavier_initializer()([self.node_size, self.rep_size]),
         #                          dtype=tf.float32, name='embeddings')
+        print(_embeddings)
 
-        Adj = Variable(torch.FloatTensor(self.node_size, self.node_size), requires_grad=False)
-        AdjMask = Variable(torch.FloatTensor(self.node_size, self.node_size), requires_grad=False)
+        Adj = Variable(torch.FloatTensor(adj_mat), requires_grad=False)
+        AdjMask = Variable(torch.Tensor(mat_mask), requires_grad=False)
         # Adj = tf.placeholder(tf.float32, [self.node_size, self.node_size], name='adj_mat')
         # AdjMask = tf.placeholder(tf.float32, [self.node_size, self.node_size], name='adj_mask')
 
