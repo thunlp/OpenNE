54468645c535d59d07acb6ef81f69e00f6347bbf
Bznkxs <gogodu@gmail.com>
2020-07-02 20:36:17 +0800

    test_ver

--- src/openne/models/node2vec.py
+++ src/openne/models/node2vec.py
@@ -9,6 +9,7 @@ class Node2vec(ModelWithEmbeddings):
 
     def __init__(self, dim=128, dw=False, **kwargs):
         super(Node2vec, self).__init__(dim=dim, dw=dw, **kwargs)
+        self.args = {}
 
     @classmethod
     def check_train_parameters(cls, **kwargs):
@@ -17,7 +18,7 @@ class Node2vec(ModelWithEmbeddings):
                                  'num_paths': 10,
                                  'p': 1.0,
                                  'q': 1.0,
-                                 'workers': 1,
+                                 'workers': 3,
                                  'min_count': 0,
                                  'sg': 1})
         return kwargs
@@ -25,21 +26,24 @@ class Node2vec(ModelWithEmbeddings):
     def build(self, graph, *, path_length=80, num_paths=10, p=1.0, q=1.0, **kwargs):
 
         if self.dw:
-            kwargs["hs"] = 1
+            self.args['hs'] = 1
             p = 1.0
             q = 1.0
+
+        self.args['p'] = p
+        self.args['q'] = q
+        self.args['workers'] = kwargs["workers"]
+
 
         if self.dw:
             self.walker = walker.BasicWalker(graph, workers=kwargs["workers"])
         else:
-            self.walker = walker.Walker(
-                graph, p=p, q=q, workers=kwargs["workers"])
+            self.walker = walker.Walker(graph, p=p, q=q, workers=kwargs["workers"])
             print("Preprocess transition probs...")
             self.walker.preprocess_transition_probs()
-        sentences = self.walker.simulate_walks(
-            num_walks=num_paths, walk_length=path_length)
-        kwargs["sentences"] = sentences
-        kwargs["size"] = kwargs.get("size", self.dim)
+        sentences = self.walker.simulate_walks(num_walks=num_paths, walk_length=path_length)
+        self.args["sentences"] = sentences
+        self.args["size"] = kwargs.get("size", self.dim)
 
     def get_train(self, graph, **kwargs):
         word2vec = Word2Vec(**kwargs)
