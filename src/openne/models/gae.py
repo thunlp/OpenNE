e1c05ffb281833663cf15509aba963213ed604e5
Charles Cui <790799494@qq.com>
2020-07-06 09:59:17 +0800

    add gae/vgae

--- src/openne/models/gae.py
+++ src/openne/models/gae.py
@@ -1,18 +1,20 @@
 import numpy as np
-from .utils import *
-from . import models
-from ..models import *
+from .gcn.utils import *
+from .gcn import models
+from .models import *
 import time
 import scipy.sparse as sp
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 
-class GCN(ModelWithEmbeddings):
+class GAE(ModelWithEmbeddings):
 
-    def __init__(self, hiddens=None, max_degree=0, **kwargs):
+    def __init__(self, output_dim=16, hiddens=None, max_degree=0, **kwargs):
         if hiddens is None:
-            hiddens = [16]
-        super(GCN, self).__init__(hiddens=hiddens, max_degree=max_degree, **kwargs)
+            hiddens = [32]
+        super(GAE, self).__init__(output_dim=output_dim, hiddens=hiddens, max_degree=max_degree, **kwargs)
 
     @classmethod
     def check_train_parameters(cls, **kwargs):
@@ -21,7 +23,7 @@ class GCN(ModelWithEmbeddings):
                                  "dropout": 0.5,
                                  "weight_decay": 1e-4,
                                  "early_stopping": 100,
-                                 "clf_ratio": 0.1,
+                                 "clf_ratio": 0.8,
                                  "hiddens": [16],
                                  "max_degree": 0})
         check_range(kwargs, {"learning_rate": (0, np.inf),
@@ -32,11 +34,11 @@ class GCN(ModelWithEmbeddings):
                              "clf_ratio": (0, 1),
                              "max_degree": (0, np.inf)})
         return kwargs
-
+    
     @classmethod
     def check_graphtype(cls, graphtype, **kwargs):
         if not graphtype.attributed():
-            raise TypeError("GCN only accepts attributed graphs!")
+            raise TypeError("GAE only accepts attributed graphs!")
 
     def build(self, graph, *, learning_rate=0.01, epochs=200,
               dropout=0.5, weight_decay=1e-4, early_stopping=100,
@@ -62,39 +64,17 @@ class GCN(ModelWithEmbeddings):
         # Create models
         input_dim = self.features.shape[1] if not self.sparse else self.features[2][1]
         feature_shape = self.features.shape if not self.sparse else self.features[0].shape[0]
-        output_dim = self.labels.shape[1]
-        self.model = models.GCNModel(input_dim=input_dim, output_dim=output_dim, hidden_dims=self.hiddens,
+
+        self.model = models.GCNModel(input_dim=input_dim, output_dim=self.output_dim, hidden_dims=self.hiddens,
                                 supports=self.support, dropout=self.dropout, sparse_inputs=self.sparse,
                                 num_features_nonzero=feature_shape, weight_decay=self.weight_decay, logging=False)
-        self.cost_val = []
+        
 
     def get_train(self, graph, **kwargs):
         # Train models
-        output, train_loss, train_acc, __ = self.evaluate(kwargs['train_mask'])
-        self.debug_info = {"train_loss": "{:.5f}".format(train_loss),
-                           "train_acc=": "{:.5f}".format(train_acc)}
-        return output
-
-    def early_stopping_judge(self, graph, *, step=0, **kwargs):
-        return step > self.early_stopping and self.cost_val[-1] > torch.mean(
-                    torch.stack(self.cost_val[-(self.early_stopping + 1):-1]))
-
-    # Define models evaluation function
-    def evaluate(self, mask, train=True):
-        torch.autograd.set_detect_anomaly(True)
-        t_test = time.time()
-        self.model.zero_grad()
-        self.model.train(train)
-        output = self.model(self.features)
-        loss = self.model.loss(self.labels, mask)
-        accuracy = self.model.accuracy(self.labels, mask)
-        if train:
-            loss.backward()
-            # print([(name, param.grad) for name,param in self.model.named_parameters()])
-            self.model.optimizer.step()
-        return output, loss, accuracy, (time.time() - t_test)
-
-    # todo: check if is standard operation for supervised node prediction
+        output, train_loss,  __ = self.evaluate(torch.ones(graph.nodesize))
+        self.debug_info = {"train_loss": "{:.5f}".format(train_loss)}
+        
     def build_label(self, graph):
         g = graph.G
         look_up = graph.look_up_dict
@@ -114,6 +94,59 @@ class GCN(ModelWithEmbeddings):
             for ll in l:
                 l_id = label_dict[ll]
                 self.labels[node_id][l_id] = 1
+    
+    def loss(self, output, adj_label, pos_weight, norm):
+        cost = 0.
+
+        cost += norm * F.binary_cross_entropy_with_logits(torch.sigmoid(torch.mm(output, output.t())), adj_label, pos_weight=adj_label * pos_weight)
+        
+        return cost 
+
+    # Define models evaluation function
+    def evaluate(self, mask, train=True):
+        torch.autograd.set_detect_anomaly(True)
+        t_test = time.time()
+        self.model.zero_grad()
+        self.model.train(train)
+        output = self.model(self.features)
+        loss = self.loss(output, self.adj_label, self.pos_weight, self.norm)
+        if train:
+            loss.backward()
+            #print([(name, param.grad) for name,param in self.model.named_parameters()])
+            self.model.optimizer.step()
+        return output, loss, (time.time() - t_test)
+
+
+
+        # # Testing
+        # test_res, test_cost, test_acc, test_duration = self.evaluate(self.test_mask, False)
+        # print("Test set results:", "cost=", "{:.5f}".format(test_cost),
+        #       "accuracy=", "{:.5f}".format(test_acc), "time=", "{:.5f}".format(test_duration))
+
+    def make_output(self, graph, **kwargs):
+        self.embeddings = self.model(self.features).detach()
+
+    def build_train_val_test(self, graph):
+        """
+            build train_mask test_mask val_mask
+        """
+        train_precent = self.clf_ratio
+        training_size = int(train_precent * graph.G.number_of_nodes())
+        state = torch.random.get_rng_state()
+        torch.random.manual_seed(0)
+        shuffle_indices = torch.randperm(graph.G.number_of_nodes())
+        torch.random.set_rng_state(state)
+        g = graph.G
+
+        def sample_mask(begin, end):
+            mask = torch.zeros(g.number_of_nodes())
+            for i in range(begin, end):
+                mask[shuffle_indices[i]] = 1
+            return mask
+
+        self.train_mask = sample_mask(0, training_size - 100)
+        self.val_mask = sample_mask(training_size - 100, training_size)
+        self.test_mask = sample_mask(training_size, g.number_of_nodes())
 
     def preprocess_data(self, graph):
         """
@@ -124,8 +157,16 @@ class GCN(ModelWithEmbeddings):
         look_back = graph.look_back_list
         self.features = torch.from_numpy(graph.features()).type(torch.float32)
         self.features = preprocess_features(self.features, sparse=self.sparse)
+
+        n = graph.nodesize
         self.build_label(graph)
+        self.build_train_val_test(graph)
+        adj_label = graph.adjmat(weighted=False, directed=False, sparse=True)
+        
+        self.adj_label = torch.FloatTensor((adj_label + sp.eye(n).toarray()))
         adj = nx.adjacency_matrix(g)  # the type of graph
+        self.pos_weight = float(n * n - adj.sum()) / adj.sum()
+        self.norm = n * n / float((n * n - adj.sum()) * 2)
         if self.max_degree == 0:
             self.support = [preprocess_adj(adj)]
         else:
