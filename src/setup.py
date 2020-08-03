0bfd6bb53be544eebe62af26c19c4dcd160fb1e1
Bznkxs <gogodu@gmail.com>
2020-08-03 11:54:16 +0800

    setup ready for package; add multi-gpu

--- src/setup.py
+++ src/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
-
+import readme_renderer
 setup(
     name="openne",
     url="https://github.com/thunlp/OpenNE",
     license="MIT",
     author="THUNLP",
-    version="2.0.0",
+    version="1.0.1",
     description="Open Source Network Embedding toolkit",
     packages=find_packages(),
     long_description=open("../README.md").read(),
@@ -17,5 +17,7 @@ setup(
         'Operating System :: OS Independent'
     ],
     python_requires='>=3.7',
-    setup_requires=[]
+    setup_requires=['torch>=1.2.0', 'six', 'numpy>=1.14',
+                    'scipy>=0.19.1', 'gensim', 'scikit-learn>=0.19.0',
+                    'networkx>=2.0', 'overloading']
 )
