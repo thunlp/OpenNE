2d9b1da7eb719c5358accf0559420eca8198a029
Bznkxs <gogodu@gmail.com>
2020-08-12 16:56:55 +0800

    line bug

--- src/openne/models/line.py
+++ src/openne/models/line.py
@@ -12,7 +12,8 @@ from .models import *
 # todo: add validation hook (see openne/line.py)
 class _LINE(ModelWithEmbeddings):
     def __init__(self, dim=128, order=2, table_size=1e8, **kwargs):
-        super(_LINE, self).__init__(dim=dim, order=order, table_size=table_size, save=False, **kwargs)
+        kwargs['save'] = False
+        super(_LINE, self).__init__(dim=dim, order=order, table_size=table_size, **kwargs)
         self.cur_epoch = 0
 
     @classmethod
